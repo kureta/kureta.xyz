938983c789f8cb06a7275f25344ae5d90e79fe57
kureta <skureta@gmail.com>
2019-09-29 13:49:45 +0000

    publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# git pull origin master
-# git submodule update --init --recursive
-# git submodule foreach git checkout master
-# git submodule foreach git pull origin master
+git pull origin master
+git submodule update --init --recursive
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
 
 rm -rf /home/kureta/srv/kureta.xyz/*
 hugo
